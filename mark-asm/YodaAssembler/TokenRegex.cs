0e5098c2e8f895ca6de26bec6143cf98e8981ac0
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-10-27 16:51:18 +0000

    Refactor: symbols inside direct/indirect brackets must start with alphabetic or underscore and be no more than 32 chars

--- mark-asm/YodaAssembler/TokenRegex.cs
+++ mark-asm/YodaAssembler/TokenRegex.cs
@@ -121,7 +121,7 @@ public static partial class TokenRegex
 	/// Detects and extracts a direct symbol from the text
 	/// </summary>
 	/// <returns></returns>
-	[GeneratedRegex(@"^\s*\[(\w{1,32})\]\s*$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^\s*\[([A-Za-z_]\w{0,31})\]\s*$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex DirectSymbol();
 	
 	/// <summary>
@@ -142,7 +142,7 @@ public static partial class TokenRegex
 	/// Detects and extracts a direct symbol from the text
 	/// </summary>
 	/// <returns></returns>
-	[GeneratedRegex(@"^\s*\[\[(\w{1,32})\]\]\s*$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^\s*\[\[([A-Za-z_]\w{0,31})\]\]\s*$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex IndirectSymbol();
 
 }
