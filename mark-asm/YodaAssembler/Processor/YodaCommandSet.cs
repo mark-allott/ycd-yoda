add01322ceae8eb089d15429a9d21ac8a6a6f72c
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-10-27 16:51:55 +0000

    Refactor: add permissible parameter types for cpu commands

--- mark-asm/YodaAssembler/Processor/YodaCommandSet.cs
+++ mark-asm/YodaAssembler/Processor/YodaCommandSet.cs
@@ -1,9 +1,12 @@
+using YodaAssembler.Enums;
 using YodaAssembler.Records;
 
 namespace YodaAssembler.Processor;
 
 public static class YodaCommandSet
 {
+	private static readonly ParameterTypes ImmediateOrDirect = ParameterTypes.LiteralNumber | ParameterTypes.Symbol |
+	                                                           ParameterTypes.DirectNumber | ParameterTypes.DirectSymbol;
 	public static readonly List<YodaCommand> Commands =
 	[
 		new YodaCommand(0x00, "halt"),
@@ -14,18 +17,18 @@ public static class YodaCommandSet
 		new YodaCommand(0x03, "nop"),
 		new YodaCommand(0x04, "sif"),
 		new YodaCommand(0x05, "cif"),
-		new YodaCommand(0x10, "savetofile", 3),
-		new YodaCommand(0x10, "stf", 3),
-		new YodaCommand(0x20, "loadfromfile", 2),
-		new YodaCommand(0x20, "lff", 2),
-		new YodaCommand(0x30, "write", 2),
-		new YodaCommand(0x40, "add", 3),
-		new YodaCommand(0x50, "sub", 3),
-		new YodaCommand(0x60, "inc", 1),
-		new YodaCommand(0x70, "dec", 1),
-		new YodaCommand(0x80, "jumpifzero", 2),
-		new YodaCommand(0x80, "jz", 2),
-		new YodaCommand(0x90, "jumpwithreturn", 1),
-		new YodaCommand(0x90, "jwr", 1),
+		new YodaCommand(0x10, "savetofile", 3, ImmediateOrDirect, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x10, "stf", 3, ImmediateOrDirect, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x20, "loadfromfile", 2, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x20, "lff", 2, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x30, "write", 2, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x40, "add", 3, ImmediateOrDirect, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x50, "sub", 3, ImmediateOrDirect, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x60, "inc", 1, ImmediateOrDirect),
+		new YodaCommand(0x70, "dec", 1, ImmediateOrDirect),
+		new YodaCommand(0x80, "jumpifzero", 2, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x80, "jz", 2, ImmediateOrDirect, ImmediateOrDirect),
+		new YodaCommand(0x90, "jumpwithreturn", 1, ImmediateOrDirect),
+		new YodaCommand(0x90, "jwr", 1, ImmediateOrDirect),
 	];
 }
