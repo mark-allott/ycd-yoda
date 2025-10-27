74a6cb7de8e8034e21bbad1f360cce22e8fdf32e
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-10-27 16:50:05 +0000

    Add implementations for new enum values

--- mark-asm/YodaAssembler/Records/YodaToken.cs
+++ mark-asm/YodaAssembler/Records/YodaToken.cs
@@ -29,22 +29,92 @@ public partial record YodaToken
 	/// Extracts a directive from the supplied text
 	/// </summary>
 	/// <returns></returns>
-	[GeneratedRegex(@"\[(\w)\]", RegexOptions.Compiled)]
+	[GeneratedRegex(@"\[(\w+)\]", RegexOptions.Compiled)]
 	private static partial Regex DirectiveRegex();
 
 	/// <summary>
 	/// Extracts a label from the supplied text
 	/// </summary>
 	/// <returns></returns>
-	[GeneratedRegex(@":([A-Za-z_]\w{1,31})\s*$", RegexOptions.Compiled)]
+	[GeneratedRegex(@":([A-Za-z]\w{0,31}|_\w{1,31})\s*$", RegexOptions.Compiled)]
 	private static partial Regex LabelRegex();
 
 	/// <summary>
 	/// Extracts a generic single-word selection from the supplied text
 	/// </summary>
 	/// <returns></returns>
-	[GeneratedRegex(@"(\w+)", RegexOptions.Compiled)]
+	[GeneratedRegex(@"([A-Za-z]\w{0,31}|_\w{1,31})", RegexOptions.Compiled)]
 	private static partial Regex GenericWordRegex();
+
+	/// <summary>
+	/// Extracts a literal string from the supplied text
+	/// </summary>
+	/// <returns></returns>
+	[GeneratedRegex(@"""(.*)""", RegexOptions.Compiled)]
+	private static partial Regex LiteralStringRegex();
+
+	/// <summary>
+	/// Extracts a literal char from the text
+	/// </summary>
+	/// <returns></returns>
+	[GeneratedRegex(@"'(\\?.)'", RegexOptions.Compiled)]
+	private static partial Regex LiteralCharRegex();
+
+	/// <summary>
+	/// Detects and extracts a numeric value from the text
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>Numbers can be expressed in either hex, binary or decimal forms:
+	/// <ul>
+	/// <li>Hex form: 0x prefix, with one or two hexadecimal digits</li>
+	/// <li>Binary form: 0b prefix, followed by 1 or 2 groups of 4-digit nibble groups, separated by an underscore, or 1 to 8 digits</li>
+	/// <li>Decimal form: no prefix, but between 1 and 3 digits</li>
+	/// </ul>
+	/// </remarks>
+	[GeneratedRegex(@"^\s*((0[Xx][\dA-Fa-f]{1,2})|(0[Bb][01]{4}(_[01]{4})?)|(0[Bb][01]{1,8})|(\d{1,3}))\s*$", RegexOptions.Compiled)]
+	private static partial Regex LiteralNumberRegex();
+
+	/// <summary>
+	/// Detects and extracts a direct numeric value from the text
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>Numbers can be expressed in either hex, binary or decimal forms, surrounded by square brackets to indicate they are direct:
+	/// <ul>
+	/// <li>Hex form: 0x prefix, with one or two hexadecimal digits</li>
+	/// <li>Binary form: 0b prefix, followed by 1 or 2 groups of 4-digit nibble groups, separated by an underscore, or 1 to 8 digits</li>
+	/// <li>Decimal form: no prefix, but between 1 and 3 digits</li>
+	/// </ul>
+	/// </remarks>
+	[GeneratedRegex(@"^\s*\[((0[Xx][\dA-Fa-f]{1,2})|(0[Bb][01]{4}(_[01]{4})?)|(0[Bb][01]{1,8})|(\d{1,3}))\]\s*$", RegexOptions.Compiled)]
+	private static partial Regex DirectNumberRegex();
+
+	/// <summary>
+	/// Detects and extracts a direct symbol from the text
+	/// </summary>
+	/// <returns></returns>
+	[GeneratedRegex(@"^\s*\[(\w{1,32})\]\s*$", RegexOptions.Compiled)]
+	private static partial Regex DirectSymbolRegex();
+
+	/// <summary>
+	/// Detects and extracts an indirect numeric value from the text
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>Numbers can be expressed in either hex, binary or decimal forms, surrounded by double square brackets to indicate indirection:
+	/// <ul>
+	/// <li>Hex form: 0x prefix, with one or two hexadecimal digits</li>
+	/// <li>Binary form: 0b prefix, followed by 1 or 2 groups of 4-digit nibble groups, separated by an underscore, or 1 to 8 digits</li>
+	/// <li>Decimal form: no prefix, but between 1 and 3 digits</li>
+	/// </ul>
+	/// </remarks>
+	[GeneratedRegex(@"^\s*\[\[((0[Xx][\dA-Fa-f]{1,2})|(0[Bb][01]{4}(_[01]{4})?)|(0[Bb][01]{1,8})|(\d{1,3}))\]\]\s*$", RegexOptions.Compiled)]
+	private static partial Regex IndirectNumberRegex();
+
+	/// <summary>
+	/// Detects and extracts a direct symbol from the text
+	/// </summary>
+	/// <returns></returns>
+	[GeneratedRegex(@"^\s*\[\[(\w{1,32})\]\]\s*$", RegexOptions.Compiled)]
+	private static partial Regex IndirectSymbolRegex();
 
 	#endregion
 
@@ -98,10 +168,15 @@ public partial record YodaToken
 			TokenType.Comment => CommentRegex(),
 			TokenType.Directive => DirectiveRegex(),
 			TokenType.Label => LabelRegex(),
-			TokenType.Command => GenericWordRegex(),
-			TokenType.Operand => GenericWordRegex(),
-			TokenType.Literal => GenericWordRegex(),
+			TokenType.Command or TokenType.Operand => GenericWordRegex(),
+			TokenType.LiteralString => LiteralStringRegex(),
+			TokenType.LiteralChar => LiteralCharRegex(),
+			TokenType.LiteralNumber => LiteralNumberRegex(),
 			TokenType.Symbol => GenericWordRegex(),
+			TokenType.DirectNumber => DirectNumberRegex(),
+			TokenType.DirectSymbol => DirectSymbolRegex(),
+			TokenType.IndirectNumber => IndirectNumberRegex(),
+			TokenType.IndirectSymbol => IndirectSymbolRegex(),
 			_ => throw new ArgumentOutOfRangeException(nameof(tokenType), tokenType, "Unhandled value")
 		};
 		var m = regex.Match(text);
@@ -115,7 +190,7 @@ public partial record YodaToken
 	/// </summary>
 	/// <param name="lineNumber">The line on which the source appears</param>
 	/// <param name="lineSequence">The position within the line</param>
-	/// <param name="text">The text representing the comment (MUST contain the semi-colon character, followed by the comment text)</param>
+	/// <param name="text">The text representing the comment (MUST contain the semicolon character, followed by the comment text)</param>
 	/// <returns>The tokenised version of the comment</returns>
 	public static YodaToken Comment(int lineNumber, int lineSequence, string text) =>
 		Create(TokenType.Comment, lineNumber, lineSequence, text);
@@ -161,14 +236,34 @@ public partial record YodaToken
 		Create(TokenType.Operand, lineNumber, lineSequence, text);
 
 	/// <summary>
-	/// Static constructor to create a "literal" token from the source
+	/// Static constructor to create a "literal" token from the source to represent a string
 	/// </summary>
 	/// <param name="lineNumber">The line on which the source appears</param>
 	/// <param name="lineSequence">The position within the line</param>
 	/// <param name="text">The text representing the literal value</param>
-	/// <returns>The tokenised version of the Literal</returns>
-	public static YodaToken Literal(int lineNumber, int lineSequence, string text) =>
-		Create(TokenType.Literal, lineNumber, lineSequence, text);
+	/// <returns>The tokenised version of the <see cref="TokenType.LiteralString"/></returns>
+	public static YodaToken LiteralString(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.LiteralString, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create a "literal" token from the source to represent a character
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the literal value</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.LiteralChar"/></returns>
+	public static YodaToken LiteralChar(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.LiteralChar, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create a "literal" token from the source to represent a number value
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the literal value</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.LiteralNumber"/></returns>
+	public static YodaToken LiteralNumber(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.LiteralNumber, lineNumber, lineSequence, text);
 
 	/// <summary>
 	/// Static constructor to create a "symbol" token from the source
@@ -176,9 +271,49 @@ public partial record YodaToken
 	/// <param name="lineNumber">The line on which the source appears</param>
 	/// <param name="lineSequence">The position within the line</param>
 	/// <param name="text">The text representing the symbol value</param>
-	/// <returns>The tokenised version of the Symbol</returns>
+	/// <returns>The tokenised version of the <see cref="TokenType.Symbol"/></returns>
 	public static YodaToken Symbol(int lineNumber, int lineSequence, string text) =>
 		Create(TokenType.Symbol, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create a "direct number" token from the source
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the number for the direct value</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.DirectNumber"/></returns>
+	public static YodaToken DirectNumber(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.DirectNumber, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create a "direct symbol" token from the source
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the number for the direct symbol</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.DirectSymbol"/></returns>
+	public static YodaToken DirectSymbol(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.DirectSymbol, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create an "indirect number" token from the source
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the number for the indirect value</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.IndirectNumber"/></returns>
+	public static YodaToken IndirectNumber(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.IndirectNumber, lineNumber, lineSequence, text);
+
+	/// <summary>
+	/// Static constructor to create an "indirect symbol" token from the source
+	/// </summary>
+	/// <param name="lineNumber">The line on which the source appears</param>
+	/// <param name="lineSequence">The position within the line</param>
+	/// <param name="text">The text representing the number for the indirect symbol</param>
+	/// <returns>The tokenised version of the <see cref="TokenType.IndirectSymbol"/></returns>
+	public static YodaToken IndirectSymbol(int lineNumber, int lineSequence, string text) =>
+		Create(TokenType.IndirectSymbol, lineNumber, lineSequence, text);
 
 	#endregion
 
@@ -199,8 +334,14 @@ public partial record YodaToken
 			TokenType.Label => $":{Text}",
 			TokenType.Command => Text,
 			TokenType.Operand => Text,
-			TokenType.Literal => Text,
+			TokenType.LiteralString => $"\"{Text}\"",
+			TokenType.LiteralChar => $"'{Text}'",
+			TokenType.LiteralNumber => Text,
 			TokenType.Symbol => Text,
+			TokenType.DirectNumber => $"[{Text}]",
+			TokenType.DirectSymbol => $"[{Text}]",
+			TokenType.IndirectNumber => $"[[{Text}]]",
+			TokenType.IndirectSymbol => $"[[{Text}]]",
 			_ => throw new ArgumentOutOfRangeException(nameof(TokenType), this.TokenType, "Unhandled value")
 		})!;
 	}
