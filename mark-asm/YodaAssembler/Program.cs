75332f340cfea1137ef3792db41b881301beaedc
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-10-27 16:48:02 +0000

    Initial sln/program for YODA assembler

---
+++ mark-asm/YodaAssembler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace YodaAssembler;
+
+internal static class Program
+{
+    private static string _filePath = null!;
+
+    internal static void Main(string[] args)
+    {
+        //	Get individual path parts to the assembly and where the assembly is in the list
+        var pathParts = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "")
+            .Split(Path.DirectorySeparatorChar)
+            .ToList();
+        var assemblyIndex = pathParts.IndexOf(nameof(YodaAssembler));
+        //	Reassemble the paths up to the point of the assembly name
+        var basePath = string.Join(Path.DirectorySeparatorChar, pathParts[..assemblyIndex]);
+        //	Append Files to the path - this is the location of the boot file area
+        _filePath = Path.Combine(basePath, "Files");
+
+        //	Make sure the Files folder exists
+        if (!Directory.Exists(_filePath))
+            Directory.CreateDirectory(_filePath);
+    }
+}
