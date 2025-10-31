866063902e4d0b834f5f4ee969c31b2fdb7d4129
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-10-31 15:19:54 +0000

    Fixup description

--- mark-asm/YodaAssembler/Interfaces/IByteCodeGeneratorStrategy.cs
+++ mark-asm/YodaAssembler/Interfaces/IByteCodeGeneratorStrategy.cs
@@ -9,7 +9,7 @@ public interface IByteCodeGeneratorStrategy
 	/// Used to generate byte code specific to a particular virtual CPU
 	/// </summary>
 	/// <param name="directiveType">The type of directive the <paramref name="tokens"/> belong to</param>
-	/// <param name="tokens">The <see cref="YodaToken"/>, or derivative, tokens to be converted to their bytecode representation</param>
+	/// <param name="tokens">The <see cref="IToken"/>, or derivative, tokens to be converted to their bytecode representation</param>
 	/// <returns>The array of bytes that represent the tokens</returns>
 	/// <remarks>If the <paramref name="tokens"/> contain any symbols, then the byte returned for it shall be null</remarks>
 	byte?[] Generate(DirectiveType directiveType, IEnumerable<IToken> tokens);
