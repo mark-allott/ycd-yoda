11b501f141d6b761e9b69dcadcd61a564d1af41a
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-06 01:00:25 +0000

    Concrete implementation of ILogger, writing output to the console

---
+++ YODA/Logging/ConsoleLogger.cs
@@ -0,0 +1,48 @@
+using SimpleInstructionMachine.Enums;
+using SimpleInstructionMachine.Interfaces;
+
+namespace SimpleInstructionMachine.Logging;
+
+public class ConsoleLogger
+	: ILogger
+{
+	#region Constructor
+
+	public ConsoleLogger(bool isDebug)
+	{
+		LogLevel = isDebug
+			? LogLevel.Debug
+			: LogLevel.Error;
+	}
+
+	#endregion
+
+	#region Properties
+
+	private LogLevel LogLevel { get; }
+
+	#endregion
+
+	#region ILogger implementation
+
+	/// <inheritdoc />
+	public void Log(LogLevel level, string message)
+	{
+		if (!IsEnabled(level))
+			return;
+
+		var consoleOutput = MessageFormatter.FormatMessage(level, message);
+		if (LogLevel >= LogLevel.Error)
+			Console.Error.WriteLine(consoleOutput);
+		else
+			Console.WriteLine(consoleOutput);
+	}
+
+	/// <inheritdoc />
+	public bool IsEnabled(LogLevel level)
+	{
+		return level >= LogLevel;
+	}
+
+	#endregion
+}
