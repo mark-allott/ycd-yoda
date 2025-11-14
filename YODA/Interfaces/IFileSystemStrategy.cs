58f3ab1214464a4338f03e98b42dca58a45111bd
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-14 18:39:20 +0000

    Refactor: add method load the bootfile

--- YODA/Interfaces/IFileSystemStrategy.cs
+++ YODA/Interfaces/IFileSystemStrategy.cs
@@ -16,7 +16,13 @@ public interface IFileSystemStrategy
 	/// <param name="fileNumber">The number for the file</param>
 	/// <returns>The contents of the specified file</returns>
 	byte[] LoadFromFile(int fileNumber);
-	
+
+	/// <summary>
+	/// Returns the contents of the bootfile from the virtual file system
+	/// </summary>
+	/// <returns>The contents of the specified file</returns>
+	byte[] LoadBootFile();
+
 	/// <summary>
 	/// Performs the writing of the specified <paramref name="contents"/> to the underlying virtual file system with the
 	/// specified <paramref name="fileNumber"/> with async support
@@ -26,7 +32,7 @@ public interface IFileSystemStrategy
 	/// <param name="token">The cancellation token</param>
 	/// <returns></returns>
 	Task SaveToFileAsync(int fileNumber, byte[] contents, CancellationToken token);
-	
+
 	/// <summary>
 	/// Returns the contents of the file named <paramref name="fileNumber"/> from the virtual file system
 	/// </summary>
@@ -34,4 +40,11 @@ public interface IFileSystemStrategy
 	/// <param name="token">The cancellation token</param>
 	/// <returns>The contents of the specified file</returns>
 	Task<byte[]> LoadFromFileAsync(int fileNumber, CancellationToken token);
+
+	/// <summary>
+	/// Returns the contents of the bootfile from the virtual file system with async support
+	/// </summary>
+	/// <param name="token">The cancellation token</param>
+	/// <returns>The contents of the specified file</returns>
+	Task<byte[]> LoadBootFileAsync(CancellationToken token);
 }
