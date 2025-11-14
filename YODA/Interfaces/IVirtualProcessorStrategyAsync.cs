cdb6cbe7eaa304d3216e06f83ba96d399da3c825
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-14 12:25:04 +0000

    Refactor: add cancellation token support

--- YODA/Interfaces/IVirtualProcessorStrategyAsync.cs
+++ YODA/Interfaces/IVirtualProcessorStrategyAsync.cs
@@ -6,26 +6,30 @@ public interface IVirtualProcessorStrategyAsync
 	/// Configures the virtual CPU prior to running
 	/// </summary>
 	/// <param name="folderPath"></param>
+	/// <param name="token">The cancellation token used to signal state to the processor</param>
 	/// <returns></returns>
-	Task Configure(string folderPath);
-	
+	Task Configure(string folderPath, CancellationToken token);
+
 	/// <summary>
 	/// Loads a pre-compiled binary program from the specified <paramref name="filename"/>
 	/// </summary>
 	/// <param name="filename">The name of the file to load into system memory</param>
+	/// <param name="token">The cancellation token used to signal state to the processor</param>
 	/// <returns></returns>
-	Task LoadProgram(string filename);
-	
+	Task LoadProgram(string filename, CancellationToken token);
+
 	/// <summary>
 	/// Loads a pre-compiled binary program from the specified byte array
 	/// </summary>
 	/// <param name="program">The pre-compiled binary program</param>
+	/// <param name="token">The cancellation token used to signal state to the processor</param>
 	/// <returns></returns>
-	Task LoadProgram(byte[] program);
-	
+	Task LoadProgram(byte[] program, CancellationToken token);
+
 	/// <summary>
 	/// Executes the program loaded into system memory
 	/// </summary>
+	/// <param name="token">The cancellation token used to signal state to the processor</param>
 	/// <returns></returns>
-	Task Run();
+	Task Run(CancellationToken token);
 }
