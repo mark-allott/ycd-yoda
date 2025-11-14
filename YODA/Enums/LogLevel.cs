5af7198543a742da049ca8fe5a5714e1f1fb83a4
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-14 16:17:46 +0000

    Refactor: add description attributes for values

--- YODA/Enums/LogLevel.cs
+++ YODA/Enums/LogLevel.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel;
+
 namespace SimpleInstructionMachine.Enums;
 
 public enum LogLevel
 {
+	[Description("Unkn")]
 	Unknown,
+	[Description("Dbug")]
 	Debug,
 	Info,
+	[Description("Warn")]
 	Warning,
+	[Description("Err")]
 	Error,
+	[Description("Crit")]
 	Critical,
+	[Description("Scrn")]
 	Screen,
 	None
 }
