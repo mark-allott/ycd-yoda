d58e104aa271dd64170f6ba2c24e17af363e4f43
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-15 16:43:46 +0000

    Refactor: remove Configure definition as it relates to a file system operation

--- YODA/Interfaces/IVirtualProcessorStrategy.cs
+++ YODA/Interfaces/IVirtualProcessorStrategy.cs
@@ -3,13 +3,6 @@ namespace SimpleInstructionMachine.Interfaces;
 public interface IVirtualProcessorStrategy
 {
 	/// <summary>
-	/// Configures the virtual CPU prior to running
-	/// </summary>
-	/// <param name="folderPath"></param>
-	/// <returns></returns>
-	void Configure(string folderPath);
-	
-	/// <summary>
 	/// Executes the program loaded into system memory
 	/// </summary>
 	/// <returns></returns>
--- YODA/Interfaces/IVirtualProcessorStrategyAsync.cs
+++ YODA/Interfaces/IVirtualProcessorStrategyAsync.cs
@@ -3,14 +3,6 @@ namespace SimpleInstructionMachine.Interfaces;
 public interface IVirtualProcessorStrategyAsync
 {
 	/// <summary>
-	/// Configures the virtual CPU prior to running
-	/// </summary>
-	/// <param name="folderPath"></param>
-	/// <param name="token">The cancellation token used to signal state to the processor</param>
-	/// <returns></returns>
-	Task Configure(string folderPath, CancellationToken token);
-
-	/// <summary>
 	/// Executes the program loaded into system memory
 	/// </summary>
 	/// <param name="token">The cancellation token used to signal state to the processor</param>
