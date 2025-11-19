f6177810de171b81f68b6d65ad974de32bd35629
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-19 14:04:53 +0000

    Refactor: not really a strategy, change to use generic

--- YODA/Interfaces/IFileSystem.cs
+++ YODA/Interfaces/IFileSystem.cs
@@ -1,6 +1,7 @@
 namespace SimpleInstructionMachine.Interfaces;
 
-public interface IFileSystemStrategy
+public interface IFileSystem<T>
+	where T : struct
 {
 	/// <summary>
 	/// Performs the writing of the specified <paramref name="contents"/> to the underlying virtual file system with the
@@ -8,20 +9,20 @@ public interface IFileSystemStrategy
 	/// </summary>
 	/// <param name="fileNumber">The number for the file</param>
 	/// <param name="contents">The contents of the file to be written</param>
-	void SaveToFile(int fileNumber, byte[] contents);
+	void SaveToFile(int fileNumber, T[] contents);
 
 	/// <summary>
 	/// Returns the contents of the file named <paramref name="fileNumber"/> from the virtual file system
 	/// </summary>
 	/// <param name="fileNumber">The number for the file</param>
 	/// <returns>The contents of the specified file</returns>
-	byte[] LoadFromFile(int fileNumber);
+	T[] LoadFromFile(int fileNumber);
 
 	/// <summary>
 	/// Returns the contents of the bootfile from the virtual file system
 	/// </summary>
 	/// <returns>The contents of the specified file</returns>
-	byte[] LoadBootFile();
+	T[] LoadBootFile();
 
 	/// <summary>
 	/// Writes the memory <paramref name="contents"/> to the crash dump file in either binary of text format
@@ -29,7 +30,7 @@ public interface IFileSystemStrategy
 	/// <param name="writeBinary">A flag indicating whether to write the binary or text representation of the crash dump file</param>
 	/// <param name="contents">The memory contents to dump</param>
 	/// <param name="instructionPointer">The current location of the instruction pointer</param>
-	void WriteCrashDump(bool writeBinary, byte[] contents, int instructionPointer);
+	void WriteCrashDump(bool writeBinary, T[] contents, int instructionPointer);
 
 	/// <summary>
 	/// Performs the writing of the specified <paramref name="contents"/> to the underlying virtual file system with the
@@ -39,7 +40,7 @@ public interface IFileSystemStrategy
 	/// <param name="contents">The contents of the file to be written</param>
 	/// <param name="token">The cancellation token</param>
 	/// <returns></returns>
-	Task SaveToFileAsync(int fileNumber, byte[] contents, CancellationToken token);
+	Task SaveToFileAsync(int fileNumber, T[] contents, CancellationToken token);
 
 	/// <summary>
 	/// Returns the contents of the file named <paramref name="fileNumber"/> from the virtual file system
@@ -47,14 +48,14 @@ public interface IFileSystemStrategy
 	/// <param name="fileNumber">The number for the file</param>
 	/// <param name="token">The cancellation token</param>
 	/// <returns>The contents of the specified file</returns>
-	Task<byte[]> LoadFromFileAsync(int fileNumber, CancellationToken token);
+	Task<T[]> LoadFromFileAsync(int fileNumber, CancellationToken token);
 
 	/// <summary>
 	/// Returns the contents of the bootfile from the virtual file system with async support
 	/// </summary>
 	/// <param name="token">The cancellation token</param>
 	/// <returns>The contents of the specified file</returns>
-	Task<byte[]> LoadBootFileAsync(CancellationToken token);
+	Task<T[]> LoadBootFileAsync(CancellationToken token);
 
 	/// <summary>
 	/// Writes the memory <paramref name="contents"/> to the crash dump file in either binary of text format with async support
@@ -63,7 +64,7 @@ public interface IFileSystemStrategy
 	/// <param name="contents">The memory contents to dump</param>
 	/// <param name="instructionPointer">The current location of the instruction pointer</param>
 	/// <param name="token">The cancellation token</param>
-	Task WriteCrashDumpAsync(bool writeBinary, byte[] contents, int instructionPointer, CancellationToken token);
+	Task WriteCrashDumpAsync(bool writeBinary, T[] contents, int instructionPointer, CancellationToken token);
 
 	/// <summary>
 	/// Provides the name of the file system's folder
