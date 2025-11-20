3dfce646a75cc9530a7440cce17d558220daeb9b
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-20 16:43:09 +0000

    Refactor: add naming of boot file and crash dump files as a concern

--- YODA/Interfaces/IFileNameStrategy.cs
+++ YODA/Interfaces/IFileNameStrategy.cs
@@ -8,4 +8,19 @@ public interface IFileNameStrategy
 	/// <param name="fileNumber">The number of the file</param>
 	/// <returns>The filename as a string, with optional file extension</returns>
 	string GetFileName(byte fileNumber);
+
+	/// <summary>
+	/// Provides the name of the boot file to be loaded
+	/// </summary>
+	string BootFileName { get; }
+
+	/// <summary>
+	/// Provides the name of the binary crash dump file
+	/// </summary>
+	string BinaryCrashDumpFileName { get; }
+
+	/// <summary>
+	/// Provides the name of the text-based crash dump file
+	/// </summary>
+	string TextCrashDumpFileName { get; }
 }
